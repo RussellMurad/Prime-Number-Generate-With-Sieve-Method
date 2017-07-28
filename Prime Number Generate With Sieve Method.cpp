cfecefd78fd9160333be9f3ef6237fd2edae8831
RussellMurad <shurjoshontan1995@gmail.com>
2017-07-28 22:51:29 +0600

    commit

---
+++ Prime Number Generate With Sieve Method.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[100] = {0};
+
+    for(int i=2; i<100; i++)
+    {
+        for(int j=i*i; j<100; j+=i)
+        {
+            arr[j-1] = 1;
+        }
+    }
+
+    for(int i=1; i<100; i++)
+    {
+        if(arr[i-1] == 0)
+            cout << i << ' ';
+    }
+
+    return 0;
+}
